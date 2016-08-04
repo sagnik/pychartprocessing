5447120173d23632b43991b0c6091008adb42cf3
sagnik <sagnik1987@gmail.com>
2016-08-04 15:02:11 -0700

    added work plan

--- linegraphproducer/codes/sineMarkerColors.py
+++ linegraphproducer/codes/sineMarkerColors.py
@@ -3,10 +3,11 @@ import matplotlib.pyplot as plt
 import sys
 
 def main():
-    marker1={'id':'o','color': ''}
+    marker1={'id':'x','color': 'darkmagenta'}
+    marker2={'id': '+', 'color': 'springgreen'}
     t = arange(0.0, 1.0, 0.01)
-    plt.plot(t,sin(2*pi*t))
-    plt.plot(t,sin(-2*pi*t))
+    plt.plot(t,sin(2*pi*t),marker=marker1['id'],color=marker1['color'])
+    plt.plot(t,sin(-2*pi*t),marker=marker2['id'],color=marker2['color'])
     plt.xlabel('some numbers')
     plt.grid(True)
     plt.ylabel('1 Hz')
