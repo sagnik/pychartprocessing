c3893ab9859aae55c489f8eac04f99ae4c22206a
sagnik <sagnik1987@gmail.com>
2016-08-04 18:12:56 -0700

    plan checked for python

--- linegraphproducer/codes/genericPlotter.py
+++ linegraphproducer/codes/genericPlotter.py
@@ -5,29 +5,109 @@ from colors import colorNames
 import matplotlib.pyplot as plt
 import sys
 from numpy import arange
-    
-def main():
-    fIndex=0
-    base = arange(0.0, 10.0, 1.0)
-    plt.plot(\
-     base,\
-     functionNames[functionNames.keys()[fIndex]](base),\
-     marker=markerNames[markerNames.keys()[fIndex]],\
-     color=colorNames[colorNames.keys()[fIndex]]\
-    )
-    #plt.plot(t,sin(-2*pi*t),marker=marker2['id'],color=marker2['color'])
-    plt.xlabel('numbers')
-    plt.grid(False)
-    plt.ylabel(functionNames.keys()[fIndex])
-    plt.title('Automated plotting from Python')
+import numpy as np
+import os
 
-    print "plotting function",functionNames.keys()[fIndex],"with marker",markerNames.keys()[fIndex],"and color",colorNames.keys()[fIndex]
-    sv=""
-    if sys.argv[1].endswith(".svg"):
-        sv=("../data/"+sys.argv[1])
-    else:
-        sv=("../data/"+sys.argv[1]+".svg")
-    plt.savefig(sv)
+baseDataDir="../data/"    
+def plotSingle(xs,functionName,markerName,colorName,combinedPlotName,axis,putGrid=False):
+    plt.plot(\
+        xs,\
+        functionNames[functionName](xs),\
+        marker=markerNames[markerName],\
+        color=colorNames[colorName],\
+        label=functionName,\
+        )
+    plt.legend(loc='upper left')
+    plt.axis(axis)
+    print "plotting function",functionName,"with marker",markerName,"and color",colorName
+    plt.grid(putGrid)
+    plt.xlabel('numbers')
+    plt.ylabel(functionName)
+    plt.title('Automated plotting from matplotlib: '+functionName)
+    directory=baseDataDir+combinedPlotName
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    svgFile=directory+"/"+combinedPlotName+"-"+functionName+"-"+markerName+"-"+colorName+".svg"    
+    plt.savefig(svgFile)
+    print "plot saved at",directory+"/"+svgFile
+
+
+def plotMultiple(xs,thisFunctionNames,thisMarkerNames,thisColorNames,combinedPlotName,axis,putGrid=False):
+    
+    for functionName in thisFunctionNames:
+        for markerName in thisMarkerNames:
+            for colorName in thisColorNames:
+                plt.plot(\
+                    xs,\
+                    functionNames[functionName](xs),\
+                    marker=markerNames[markerName],\
+                    color=colorNames[colorName],\
+                    label=functionName+"-"+markerName+"-"+colorName 
+                )
+                print "plotting function",functionName,"with marker",markerName,"and color",colorName
+
+    plt.legend(loc='upper left')
+    plt.axis(axis)
+    plt.grid(putGrid)
+    plt.xlabel('numbers')
+    plt.ylabel("functions")
+    plt.title('Automated plotting from matplotlib: '+"different functions")
+
+    directory=baseDataDir+combinedPlotName
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
+    svgFile=combinedPlotName+".svg"
+    metFile=combinedPlotName+".met"    
+    plt.savefig(directory+"/"+svgFile)
+
+    with open(directory+"/"+metFile,"w") as f:
+        for colorName in thisColorNames:
+            f.write(colorName+"\n")
+
+    print "plot saved at",baseDataDir+combinedPlotName+".svg"                      
+
+def main():
+    combinedPlotName="test"
+
+    indices=[0,1]
+    base = arange(0.0, 10.0, 1.0)
+
+    thisFunctionNames=[functionNames.keys()[index] for index in indices]
+    thisColorNames=[colorNames.keys()[index] for index in indices]
+    thisMarkerNames=[markerNames.keys()[index] for index in indices]
+
+    fValues=[functionNames[functionName](base) for functionName in thisFunctionNames]
+    axis=[np.min(base),np.max(base),np.min(fValues),np.max(fValues)]
+    
+    for functionName in thisFunctionNames:
+        for markerName in thisMarkerNames:
+            for colorName in thisColorNames:
+                plotSingle(\
+                xs=base,\
+                functionName=functionName,\
+                markerName=markerName,\
+                colorName=colorName,\
+                combinedPlotName=combinedPlotName,\
+                axis=axis,\
+                putGrid=False\
+                )
+                plt.clf()    
+    
+    print "\n--------------------------\n"
+    print "plotting multiple functions"
+    print "\n--------------------------\n"
+
+    plotMultiple(\
+        xs=base,\
+        thisFunctionNames=thisFunctionNames,\
+        thisMarkerNames=thisMarkerNames,\
+        thisColorNames=thisColorNames,\
+        combinedPlotName=combinedPlotName,\
+        axis=axis,\
+        putGrid=False\
+        )
+    plt.clf()
 
 if __name__ == "__main__":
     main()
--- notes/PLAN.md
+++ notes/PLAN.md
@@ -2,9 +2,9 @@
 
 #Python: 
 * [x] Define a set of 1. functions, 2. markers, 3. [colors](http://stackoverflow.com/questions/22408237/named-colors-in-matplotlib).
-* [ ] Choose a set of `<function,marker,color>` triples.
-* [ ] For each item in the set above, plot and save a figure of the form `<Figurename>-Curve-<color>.svg`.
-* [ ] Plot and save the set in `<Figurename>.svg`. Generate a metadata file with color names `<Figurename>-Colors.met`.
+* [x] Choose a set of `<function,marker,color>` triples.
+* [x] For each item in the set above, plot and save a figure of the form `<Figurename>-Curve-<color>.svg`.
+* [x] Plot and save the set in `<Figurename>.svg`. Generate a metadata file with color names `<Figurename>-Colors.met`.
 
 #Scala
 * [ ] For each file of the form <Figurename>-Curve-<color>.svg create an `atomic SVG`: `<Figurename>-Curve-<color>-sps.svg`.
