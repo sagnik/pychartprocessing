4499ef0bf5c9af86a09f1d31cfcfb56e65e0dfa2
sagnik <sagnik1987@gmail.com>
2016-09-06 21:38:30 -0400

    added code for generating configs

---
+++ linegraphproducer/codes/configProducer.py
@@ -0,0 +1,28 @@
+import sys
+
+def main():
+    twoFunctions=\
+"""<?xml version="1.0"?>
+   <!-- generated automatically, do not edit -->
+    <config>
+        <plot>
+            <function>{0}</function>
+            <marker>{1}</marker>
+            <color>{2}</color>
+        </plot>
+        <plot>
+            <function>{3}</function>
+            <marker>{4}</marker>
+            <color>{5}</color>
+        </plot>
+    </config>
+</xml>"""
+    for i in range(10):
+        with open("../configs/{0}.config".format(i),"w") as f:
+            f.write(twoFunctions.format(i,i,i,i+1,i+1,i+1))
+            
+    
+
+if __name__ == "__main__":
+    main() 
+
