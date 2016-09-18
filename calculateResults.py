0da72ac4b1ba8d6717746e8f31ce4cfcd1eb83d3
sagnik <sagnik1987@gmail.com>
2016-09-18 01:34:29 -0400

    first run precision, recall, f1score calculated

--- calculateResults.py
+++ calculateResults.py
@@ -10,11 +10,16 @@ def main():
     f1scores=[]
     for j in jsons:
         jc=json.load(open(os.path.join(sys.argv[1],j)))
-        
         for item in jc:
             precisions.append(item['precision'])
             recalls.append(item['recall'])
-            f1scores.append(item['f1score']) 
-            
+            f1scores.append(item['f1score'])
+     
+    print "average precision",np.average(precisions)        
+    print "average recall",np.average(recalls)        
+    print "average f1 score",np.average(f1scores)        
  
+if __name__ == "__main__":
+    main()       
+
 
